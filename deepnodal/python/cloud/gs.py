2c2eab4d4c31c8bd94c34610f5ae35cdcc637331
Bhumbra <bhumbra@gmail.com>
2020-04-14 14:38:36 +0100

    Support cloud reading/writing with temporary intermediaries

--- deepnodal/python/cloud/gs.py
+++ deepnodal/python/cloud/gs.py
@@ -4,6 +4,8 @@ import os
 import google.cloud.storage
 from googleapiclient import discovery
 
+TMP_DIR = '/tmp'
+
 #-------------------------------------------------------------------------------
 class GCS:
   prefix=None
@@ -19,18 +21,68 @@ class GCS:
     self.prefix = None
     self.bucket = None
     if not self.project: return self.bucket
+    if self.project[:5] == 'gs://':
+      self.project = self.project[:5].split('/')[0]
     self.prefix = 'gs://{}'.format(self.project)
     client = google.cloud.storage.Client(*args, **kwds)
     self.bucket = client.get_bucket(self.project)
     return self.bucket
 
 #-------------------------------------------------------------------------------
+  def ret_tmp_dir(self, path):
+    assert path[-1] == '/', "Last character must be '/'"
+    if path[0] != '/':
+      path = '/' + path
+    return TMP_DIR + path
+
+#-------------------------------------------------------------------------------
   def __call__(self, function=None, *args, **kwds):
+    """
+    If args is entered, it must be contain exactly 2 string elements:
+    args[0] = absolute path of file to read or write (excluding project)
+    args[1] = either 'rb' or 'wb' weather reading or writing
+    """
     if function is None:
       return
+    assert len(args) == 2, "{} args entered, 2 expected".format(len(args))
+    assert isinstance(args[0], str), "args[0] is not a string"
+    assert isinstance(args[1], str), "args[1] is not a string"
+
     if self.bucket is None:
-      return function(*args, **kwds)
-    pass
+      if args[1] in ['rb', 'wb']:
+        with open(*args) as open_bin:
+          out = function(open_bin, **kwds)
+        return out
+      return function(args[0], **kwds)
+    
+    if path[0] != '/':
+      path = '/' + path
+    directory, filename = os.path.split(path)
+    tmp_dir = self.ret_tmp_dir(directory + '/')
+    tmp_path = os.join(tmp_dir, filename)
+    if not os.path.isdir(tmp_dir):
+      os.mkdir(tmp_dir)
+
+    # Download then read
+    if args[1] in ['rb', 'read']:
+      self.download(args[0], tmp_dir)
+      if args[1] == 'rb':
+        with open(tmp_path, args[1]) as read_bin:
+          out = function(read_bin, **kwds)
+        return out
+      return function(tmp_path,  **kwds)
+
+    # Write then upload
+    if args[1] in ['wb', 'write']:
+      if args[1] == 'wb':
+        with open(tmp_path, args[1]) as write_bin:
+          out = function(write_bin, **kwds)
+      else:
+        out = function(tmp_path, **kwds)
+      self.upload(tmp_dir, filename, directory)
+      return out
+
+    raise ValueError("Unknown args[1] spec: {}".format(args[1]))
 
 #-------------------------------------------------------------------------------
   def upload(self, source_dir, filenames, dest_dir=None):
